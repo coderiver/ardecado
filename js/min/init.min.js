d2ca7c8711dec4f7561a38703156b5a8059e117b
Vadim Graboviy <hraboviyvadim@gmail.com>
2015-08-25 14:17:37 +0300

    gulp template init

--- js/min/init.min.js
+++
@@ -1,6 +0,0 @@
-head.load("js/lib/jquery.js", 
-					"js/lib/slick.min.js",
-					"js/lib/jquery.custom-scrollbar.js",
-					"js/lib/bootstrap.min.js",
-					"js/lib/nouislider.min.js",
-					"js/common.js");
---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+//= ../../bower_components/jquery/dist/jquery.min.js
+//= ../../bower_components/slick-carousel/slick/slick.min.js
+//= partials/bootstrap.min.js
+//= partials/jquery.custom-scrollbar.js
+//= partials/nouislider.min.js
+//= partials/head.js
+
+//= app.js
