42a4b33be636a46dadfff54612bbb3660192b9a9
Vadim Graboviy <hraboviyvadim@gmail.com>
2015-08-26 15:20:31 +0300

    minor

--- config.rb
+++ config.rb
@@ -7,7 +7,8 @@ sass_dir = "sass"
 images_dir = "img"
 javascripts_dir = "js"
 
-
+# sourcemap enable
+sourcemap = true
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
 output_style = :compact
