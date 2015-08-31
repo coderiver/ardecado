ba6d5a53014dff7f6af6059e57cc6e4b181f0afd
Vadim Graboviy <hraboviyvadim@gmail.com>
2015-08-31 18:31:56 +0300

    select upd

--- js/select.js
+++ js/select.js
@@ -1,6 +1,24 @@
 head.ready(function() {
 
-	
-	
+	$('.select').each(function() {
+		var select = $(this),
+			placeholder = select.attr('placeholder');
+
+		select.multipleSelect({
+			maxHeight: 1000,
+			single: true,
+			placeholder: placeholder,
+			onOpen: function(view) {
+				var droplist = select.parent().find('.ms-drop');
+				if (!droplist.hasClass('is-open')) {
+					droplist.customScrollbar();
+					droplist.addClass('is-open');
+				};
+				
+			}
+		});
+
+	});
+
 
 });
