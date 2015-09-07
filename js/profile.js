de11d78b684d8be43597e28daf4ba307aec2a985
Vadim Graboviy <hraboviyvadim@gmail.com>
2015-09-07 18:11:40 +0300

    upd

---
+++ js/profile.js
@@ -0,0 +1,37 @@
+head.ready(function() {
+
+	function tab() {
+		$('.js-orders-group').each(function(){
+			var tab_cont = $(this).find('.js-orders-cont'),
+				tab_link = $(this).find('.js-orders-link');
+
+			tab_link.first().addClass('is-active');
+			tab_cont.hide();
+			tab_cont.first().show();
+
+			$('body').on('click', '.js-orders-link', function(){
+				var link = $(this).attr('href');
+				var activeTab = $(this).parents('.js-orders-group').find('.' + link);
+				$(this).parents('.js-orders-group').find('.js-orders-link').removeClass('is-active');
+				$(this).addClass('is-active');
+				$(this).parents('.js-orders-group').find('.js-orders-cont').hide();
+				activeTab.show();
+
+				var customScroll = activeTab.find('.js-scroll-init');
+
+				if (customScroll.length) {
+					if (!customScroll.hasClass('is-loaded')) {
+						customScroll.customScrollbar();
+						customScroll.addClass('is-loaded');
+					};
+				};
+
+				return false;
+			});
+		});
+	}
+	tab();
+
+
+
+});
