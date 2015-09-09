c2c4417fc6a1f871b10a493b796d1abfc9d77083
Vadim Graboviy <hraboviyvadim@gmail.com>
2015-09-09 10:05:22 +0300

    tabs upd

--- js/common.js
+++ js/common.js
@@ -349,14 +349,14 @@ head.ready(function() {
 			var tab_cont = $(this).find('.js-tab-cont'),
 				tab_link = $(this).find('.js-tab-link');
 
-			//tab_link.first().addClass('is-active');
-			//tab_cont.hide();
-			//tab_cont.first().show();
+			tab_link.first().addClass('is-active');
+			tab_cont.hide();
+			tab_cont.first().show();
 
-			$('body').on('click', '.btn-tab, .js-tab-link', function(){
+			$('body').on('click', '.js-tab-link', function(){
 				var link = $(this).attr('href');
 				var activeTab = $(this).parents('.js-tab-group').find('.' + link);
-				$(this).parents('.js-tab-group').find('.btn-tab,.js-tab-link').removeClass('is-active');
+				$(this).parents('.js-tab-group').find('.js-tab-link').removeClass('is-active');
 				$(this).addClass('is-active');
 				$(this).parents('.js-tab-group').find('.js-tab-cont').hide();
 				activeTab.show();
